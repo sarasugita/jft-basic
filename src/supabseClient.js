4ab0d6a2e379f6d46ff12b451859f1f3d233c771
sara-sugita-1231 <ssugita@bu.edu>
2026-02-04 21:28:58 -0500

    added supabase

---
+++ src/supabseClient.js
@@ -0,0 +1,11 @@
+// src/supabaseClient.js
+import { createClient } from "@supabase/supabase-js";
+
+const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
+const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY;
+
+if (!supabaseUrl || !supabaseAnonKey) {
+  console.error("Missing Supabase env vars. Check .env and restart vite.");
+}
+
+export const supabase = createClient(supabaseUrl, supabaseAnonKey);
