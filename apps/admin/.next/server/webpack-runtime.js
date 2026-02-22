f2487896d70a53bdab52083b355cf7a3b5bfc82e
sara-sugita-1231 <ssugita@bu.edu>
2026-02-22 10:54:26 -0500

    change result display

--- apps/admin/.next/server/webpack-runtime.js
+++ apps/admin/.next/server/webpack-runtime.js
@@ -125,7 +125,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	(() => {
-/******/ 		__webpack_require__.h = () => ("c321e390c8399cf6")
+/******/ 		__webpack_require__.h = () => ("fc7b4d9d214bb13a")
 /******/ 	})();
 /******/ 	
 /******/ 	/* webpack/runtime/hasOwnProperty shorthand */
--- apps/admin/.next/static/chunks/webpack.js
+++ apps/admin/.next/static/chunks/webpack.js
@@ -192,7 +192,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "b24b6c9485ea7dbe"; }
+/******/ 		__webpack_require__.h = function() { return "68825072f1dcd53e"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
