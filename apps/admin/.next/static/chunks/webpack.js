827dce7c256b65f32457cc08e31eaa692d75524f
sara-sugita-1231 <ssugita@bu.edu>
2026-02-22 10:57:57 -0500

    add new changes

--- apps/admin/.next/static/chunks/webpack.js
+++ apps/admin/.next/static/chunks/webpack.js
@@ -192,7 +192,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "68825072f1dcd53e"; }
+/******/ 		__webpack_require__.h = function() { return "93277e260e5c8a72"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
