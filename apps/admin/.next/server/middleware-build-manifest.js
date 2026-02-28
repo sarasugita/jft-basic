129192ff2c40fcc93b2281cb0bec393bd857ba09
sara-sugita-1231 <ssugita@bu.edu>
2026-02-28 00:12:22 -0500

    implement audit logs analytics dashboard 2

--- apps/admin/.next/server/middleware-build-manifest.js
+++ apps/admin/.next/server/middleware-build-manifest.js
@@ -1,1 +1,21 @@
-self.__BUILD_MANIFEST={polyfillFiles:["static/chunks/polyfills-42372ed130431b0a.js"],devFiles:[],ampDevFiles:[],lowPriorityFiles:[],rootMainFiles:["static/chunks/webpack-c105cf7135d99ba2.js","static/chunks/1dd3208c-eb3314d020cfe930.js","static/chunks/528-981e13e369357a4f.js","static/chunks/main-app-72fc04817889c8f3.js"],pages:{"/_app":["static/chunks/webpack-c105cf7135d99ba2.js","static/chunks/framework-3664cab31236a9fa.js","static/chunks/main-b96d8805f0fc65a5.js","static/chunks/pages/_app-10a93ab5b7c32eb3.js"],"/_error":["static/chunks/webpack-c105cf7135d99ba2.js","static/chunks/framework-3664cab31236a9fa.js","static/chunks/main-b96d8805f0fc65a5.js","static/chunks/pages/_error-2d792b2a41857be4.js"]},ampFirstPages:[]},self.__BUILD_MANIFEST.lowPriorityFiles=["/static/"+process.env.__NEXT_BUILD_ID+"/_buildManifest.js",,"/static/"+process.env.__NEXT_BUILD_ID+"/_ssgManifest.js"];
+self.__BUILD_MANIFEST = {
+  "polyfillFiles": [
+    "static/chunks/polyfills.js"
+  ],
+  "devFiles": [],
+  "ampDevFiles": [],
+  "lowPriorityFiles": [],
+  "rootMainFiles": [
+    "static/chunks/webpack.js",
+    "static/chunks/main-app.js"
+  ],
+  "pages": {
+    "/_app": []
+  },
+  "ampFirstPages": []
+};
+self.__BUILD_MANIFEST.lowPriorityFiles = [
+"/static/" + process.env.__NEXT_BUILD_ID + "/_buildManifest.js",
+,"/static/" + process.env.__NEXT_BUILD_ID + "/_ssgManifest.js",
+
+];
